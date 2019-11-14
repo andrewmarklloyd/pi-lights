d1b60680b2e76b200c100280eef1ee4e7a63a725
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-11-14 09:17:58 -0800

    Hack for adding second pi to send http requests to

--- main.go
+++ main.go
@@ -2,18 +2,28 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"net/http"
 	"os"
 	"os/signal"
+	"strconv"
+	"strings"
 	"syscall"
+	"time"
 
 	"github.com/stianeikeland/go-rpio"
 )
 
 var pin rpio.Pin
+var pinNumber int
+var hostname string
 
 func main() {
-	pin = rpio.Pin(4)
+	hostname, _ = os.Hostname()
+	data, _ := ioutil.ReadFile("./config")
+	s := strings.Trim(string(data), "\n")
+	pinNumber, _ := strconv.Atoi(s)
+	pin = rpio.Pin(pinNumber)
 
 	err := rpio.Open()
 	if err != nil {
@@ -25,30 +35,60 @@ func main() {
 	signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 	go func() {
 		<-c
-		cleanup(pin)
+		cleanup(pin, pinNumber)
 		os.Exit(1)
 	}()
 
 	pin.Output()
 	fmt.Println("Setting up http handlers")
 	http.HandleFunc("/switch", switchHandler)
+	http.HandleFunc("/pin", pinHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
 func switchHandler(w http.ResponseWriter, req *http.Request) {
 	pin.Toggle()
 	pinStatus := pin.Read()
+	var op string
 	fmt.Println("pin", pinStatus)
-
+	var status string
 	if pinStatus == 0 {
-		fmt.Fprintf(w, "<h1>OFF</h1>")
+		status = "<h1>OFF</h1>"
+		op = "off"
 	} else if pinStatus == 1 {
-		fmt.Fprintf(w, "<h1>ON</h1>")
+		status = "<h1>ON</h1>"
+		op = "on"
 	}
+
+	if hostname != "zero" {
+		httpReq, _ := http.NewRequest("GET", "http://192.168.0.115:8080/pin", nil)
+		httpReq.Header.Set("op", op)
+		var client = http.Client{Timeout: time.Second * 10}
+		resp, err := client.Do(httpReq)
+		if err != nil {
+			fmt.Println(err)
+		} else {
+			fmt.Println(resp.StatusCode)
+		}
+	}
+	fmt.Fprintf(w, status)
 }
 
-func cleanup(pin rpio.Pin) {
-	fmt.Println("Cleaning up")
+func pinHandler(w http.ResponseWriter, req *http.Request) {
+	op := req.Header.Get("op")
+	var status string
+	if op == "on" {
+		pin.Write(rpio.High)
+		status = "ON"
+	} else if op == "off" {
+		pin.Write(rpio.Low)
+		status = "OFF"
+	}
+	fmt.Fprintf(w, status)
+}
+
+func cleanup(pin rpio.Pin, pinNumber int) {
+	fmt.Println("Cleaning up pin", pinNumber)
 	pin.Write(rpio.Low)
 	rpio.Close()
 }
