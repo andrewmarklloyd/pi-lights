83c48f7351c8e9346f24d2fb959bfa49a558f7d4
Andrew Lloyd <andrew85.lloyd@gmail.com>
2019-11-28 13:52:16 -0800

    output latest version

--- install/update.sh
+++ install/update.sh
@@ -14,6 +14,7 @@ rm -f ./static/*
 cp ${archive_path}/install/* install/
 cp ${archive_path}/static/* static/
 echo ${latestVersion} > /home/pi/static/version
+echo ${latestVersion} > /home/pi/static/latestVersion
 mv ${archive_path}/lights ./
 rm -rf ${archive_path}
 sudo systemctl restart lights.service
